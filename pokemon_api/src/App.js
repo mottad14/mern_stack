189d33683df6928213d7344149c86c43d183ac0b
Daniel Motta <mottad14@gmail.com>
2022-05-11 13:53:48 -0500

    comment from mike

--- pokemon_api/src/App.js
+++ pokemon_api/src/App.js
@@ -3,6 +3,7 @@ import PokeCard from './components/PokeCard';
 
 function App() {
   return (
+    // comment from mike
     <div className="App">
      <h1>Make the strongest pokemon team!</h1>
      <PokeCard name="bidoof"></PokeCard> Our Component is Here.
