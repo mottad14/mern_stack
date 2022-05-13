d658a95edf7973099426fb32883b1cb6f6c2ffea
Daniel Motta <mottad14@gmail.com>
2022-05-13 13:44:53 -0500

    Testing out promises JS

--- Promises.js
+++ Promises.js
@@ -1,11 +1,31 @@
-const noMondays = new Promise( (resolve, reject) => {
-    if(2 !== 1) {
-        resolve("Good, it's not Monday!");
-    } else {
-        reject("Someone has a case of the Mondays!");
-    }
-});
-noMondays
-    .then( res => console.log(res) )
-    .catch( err => console.log(err) );
+// const noMondays = new Promise( (resolve, reject) => {
+//     if(2 !== 1) {
+//         resolve("Good, it's not Monday!");
+//     } else {
+//         reject("Someone has a case of the Mondays!");
+//     }
+// });
+// noMondays
+//     .then( res => console.log(res) )
+//     .catch( err => console.log(err) );
 
+
+
+
+function fiveHeadsSync() {
+    let headsCount = 0;
+    let attempts = 0;
+    while(headsCount < 5) {
+        attempts++;
+        let result = tossCoin();
+        console.log(`${result} was flipped`);
+        if(result === "heads") {
+            headsCount++;
+        } else {
+            headsCount = 0;
+        }
+    }
+    return `It took ${attempts} tries to flip five "heads"`;
+}
+console.log( fiveHeadsSync() );
+console.log( "This is run after the fiveHeadsSync function completes" );
