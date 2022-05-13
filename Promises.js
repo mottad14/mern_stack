7c2da9b0e810c808c021cfc08f4d2eabd5282715
Daniel Motta <mottad14@gmail.com>
2022-05-13 15:07:27 -0500

    Coin Flip Assignment

--- Promises.js
+++ Promises.js
@@ -10,22 +10,59 @@
 //     .catch( err => console.log(err) );
 
 
+// Your mission is to rewrite the function using Promises. 
+// Make sure your fiveHeads function will call the resolve function when 
+// the coin has flipped "heads" five times in a row.
 
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
 
-function fiveHeadsSync() {
-    let headsCount = 0;
-    let attempts = 0;
-    while(headsCount < 5) {
-        attempts++;
-        let result = tossCoin();
-        console.log(`${result} was flipped`);
-        if(result === "heads") {
-            headsCount++;
-        } else {
+// function fiveHeadsSync() {
+//     let headsCount = 0;
+//     let attempts = 0;
+//     while(headsCount < 5) {
+//         attempts++;
+//         let result = tossCoin();
+//         console.log(`${result} was flipped`);
+//         if(result === "heads") {
+//             headsCount++;
+//         } else {
+//             headsCount = 0;
+//         }
+//     }
+//     return `It took ${attempts} tries to flip five "heads"`;
+// }
+
+function fiveHeads() {
+    return new Promise( (resolve, reject) => {
+        let headsCount = 0;
+        var attempts = 0;
+        let max = 100;
+        while(headsCount < 5 && attempts <= max){
+            attempts++;
+            console.log(attempts);
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if(result === "heads") {
+                headsCount++;
+            
+            } else {
             headsCount = 0;
         }
     }
-    return `It took ${attempts} tries to flip five "heads"`;
-}
-console.log( fiveHeadsSync() );
-console.log( "This is run after the fiveHeadsSync function completes" );
+        console.log(attempts)
+        if (attempts <=max){
+            resolve( `It took ${attempts} tries to flip five "heads"`);    
+        } 
+        else{
+            reject( `After ${max} tries - we were unable to flip five "heads" in a row.`);    
+        }
+    })
+};
+
+fiveHeads()
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
+// console.log( "When does this run now?" );
+
