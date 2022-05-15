1281b7131f7e1ad6024e258af411332484c41e75
Daniel Motta <mottad14@gmail.com>
2022-05-15 15:56:21 -0500

    Using Axios now to fetch "GET" data

--- fetch_pokemon/src/App.js
+++ fetch_pokemon/src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, {useState} from "react";
+import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
-
+  
   const getPokemon = ()=>{
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-    .then(data => data.json())
-    .then(response=> 
-      {
-        console.log(response);
-        setPokemon(response.results);
-      })
-    .catch(error=>console.log(error))
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').then(response=>{
+      console.log(response.data.results)
+      setPokemon(response.data.results)})
   }
+  //We can use the fetch method as shown below or use Axios as shown above
+  // const getPokemon = ()=>{
+  //   fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+  //   .then(data => data.json())
+  //   .then(response=> 
+  //     {
+  //       console.log(response);
+  //       setPokemon(response.results);
+  //     })
+  //   .catch(error=>console.log(error))
+  // }
 
   return (
     <div className="App">
