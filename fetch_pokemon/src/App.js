033d1630fabc62b6351169e535d59466acf51505
Daniel Motta <mottad14@gmail.com>
2022-05-15 15:18:12 -0500

    Gets all Pokemon Names

--- fetch_pokemon/src/App.js
+++ fetch_pokemon/src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, {useState} from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
 
+  const getPokemon = ()=>{
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    .then(data => data.json())
+    .then(response=> 
+      {
+        console.log(response);
+        setPokemon(response.results);
+      })
+    .catch(error=>console.log(error))
+  }
 
   return (
     <div className="App">
-      
+        <button onClick={getPokemon} className='btn btn-info btn-lg d-block mx-auto my-2'>Get Pokemon!</button>
+      <ul>
+      {
+      pokemon.map((pokemon, index)=>{
+        return (<li key={index}> {pokemon.name} </li>)
+      })
+      }
+      </ul>
     </div>
   );
 }
