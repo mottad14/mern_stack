2da361dea34470a87123f0e1cba2ab8a3b2461de
Daniel Motta <mottad14@gmail.com>
2022-05-16 13:53:52 -0500

    useParams to change City name

--- routing_app/src/App.js
+++ routing_app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useParams } from "react-router";
 import {
   BrowserRouter,
   Switch,
@@ -7,38 +8,70 @@ import {
   Link 
 } from "react-router-dom";
 
-const Home = (props) =>{
-    return (
-      <h1 style={{color: "red"}}> Home Component </h1>
-    )
-  }
+// Example of Routing to Home and About Page:
 
-const About = (props) =>{
-  return(
-    <h1 style={{color: "blue"}}> About Component </h1>
-  )
+// const Home = (props) =>{
+//     return (
+//       <h1 style={{color: "red"}}> Home Component </h1>
+//     )
+//   }
+
+// const About = (props) =>{
+//   return(
+//     <h1 style={{color: "blue"}}> About Component </h1>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <h1>Routing Example</h1>
+//       <p>
+//         <Link to="/">Home</Link>
+//          | 
+//         <Link to="/about">About</Link>   
+//       </p>
+
+//       The Switch and Route lines below will help us move between the 2 routes (About and Home page), seamlessly. 
+//       <Switch>
+//         <Route path="/about">  <About/>  </Route>
+        
+//         <Route exact path="/">  <Home/>  </Route>
+
+//       </Switch>
+
+//     </BrowserRouter>
+//   );
+// }
+
+
+// Example of Routing with useParams to use Variables - :city - indicate where our routing prop/variable is
+const Location = (props) => {
+
+  const { city } = useParams();
+  return (
+    <h1> City Component loaded - Welcome to {city}'s page! </h1>
+  );
 }
-
+    
 function App() {
   return (
     <BrowserRouter>
-      <h1>Routing Example</h1>
       <p>
-        <Link to="/">Home</Link>
-         | 
-        <Link to="/about">About</Link>   
+        <Link to="/location/seattle">Seattle</Link>
+        &nbsp;|&nbsp;
+        <Link to="/location/chicago">Chicago</Link>
+        &nbsp;|&nbsp;
+        <Link to="/location/burbank">Burbank</Link>
       </p>
-
-      The Switch and Route lines below will help us move between the 2 routes (About and Home page), seamlessly. 
       <Switch>
-        <Route path="/about">  <About/>  </Route>
-        
-        <Route exact path="/">  <Home/>  </Route>
-
+        <Route path="/location/:city">
+          <Location />
+        </Route>
       </Switch>
-
     </BrowserRouter>
   );
 }
+    
 
 export default App;
