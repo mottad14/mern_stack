3f4d580b6ab921fe9e2d207f31b634145822baad
Daniel Motta <mottad14@gmail.com>
2022-05-16 13:19:50 -0500

    Assignment Updates

--- routing_app/src/App.js
+++ routing_app/src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link 
+} from "react-router-dom";
+
+const Home = (props) =>{
+    return (
+      <h1 style={{color: "red"}}> Home Component </h1>
+    )
+  }
+
+const About = (props) =>{
+  return(
+    <h1 style={{color: "blue"}}> About Component </h1>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Routing Example</h1>
+      <p>
+        <Link to="/">Home</Link>
+         | 
+        <Link to="/about">About</Link>   
+      </p>
+
+      The Switch and Route lines below will help us move between the 2 routes (About and Home page), seamlessly. 
+      <Switch>
+        <Route path="/about">  <About/>  </Route>
+        
+        <Route exact path="/">  <Home/>  </Route>
+
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
