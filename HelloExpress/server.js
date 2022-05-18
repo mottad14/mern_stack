6cc5be0f88b6f186f3ea2637c42daf5b17654fd1
Daniel Motta <mottad14@gmail.com>
2022-05-18 17:54:25 -0500

    Server settings

--- HelloExpress/server.js
+++ HelloExpress/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const port = 8000;
 
 // req is short for request
 // res is short for response
 app.get("/api", (req, res) => {
   res.send("Our express api server is now sending this over to the browser");
+  res.json({ message: "Hello World" });
 });
 
 const server = app.listen(8000, () =>
